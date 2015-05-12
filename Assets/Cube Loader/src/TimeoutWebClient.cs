18f2143860a6c6a52a96be56900b68248bee9c88
Stefan Gordon <stefan@stefangordon.com>
2015-05-12 10:36:55 -0700

    TimeoutWebClient

---
+++ Assets/Cube Loader/src/TimeoutWebClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Assets.Cube_Loader.src
+{
+    public class TimeoutWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = 2000;
+            return w;
+        }
+    }
+}
