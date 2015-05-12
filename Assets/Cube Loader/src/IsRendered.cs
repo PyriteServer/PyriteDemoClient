88f2c12993861a9c479f267826654e98b5b234e0
Andre Price <obsoleted@users.noreply.github.com>
2015-05-12 12:33:22 -0700

    When upgrading remove old model after new ones are loaded

--- Assets/Cube Loader/src/IsRendered.cs
+++ Assets/Cube Loader/src/IsRendered.cs
@@ -2,6 +2,7 @@
 {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
     using UnityEngine;
 
@@ -17,18 +18,19 @@
         private int _x, _y, _z;
         private LoadCubeRequest _loadCubeRequest;
         private bool _upgraded = false;
+        private bool _upgrading = false;
 
         private bool Upgradable
         {
             get
             {
-                return !_upgraded && _manager != null && _childDetectors.Count == 0 && _pyriteQuery.DetailLevels.ContainsKey(_lod - 1);
+                return !_upgraded && !_upgrading && _manager != null && _childDetectors.Count == 0 && _pyriteQuery.DetailLevels.ContainsKey(_lod - 1);
             }
         }
 
         private bool Downgradable
         {
-            get { return _upgraded; }
+            get { return _upgraded && !_upgrading; }
         }
 
         public void SetCubePosition(int x, int y, int z, int lod, PyriteQuery query, PyriteLoader manager)
@@ -79,8 +81,10 @@
 
         private IEnumerator RequestCubeLoad()
         {
+            CancelRequest();
             _loadCubeRequest = new LoadCubeRequest(_x, _y, _z, _lod, _pyriteQuery, createdObject =>
             {
+                DestroyChildren();
                 _cubes.Add(createdObject);
                 StartCoroutine(StopRenderCheck(Camera.main));
             });
@@ -108,39 +112,91 @@
         {
             detectorCubeToRelease.name = "Released: " + detectorCubeToRelease.name;
             detectorCubeToRelease.GetComponent<IsRendered>()._upgraded = false;
+            detectorCubeToRelease.GetComponent<IsRendered>()._upgrading = false;
             detectorCubeToRelease.SetActive(false);
         }
 
-        private void DestroyChildren()
+        private void CancelRequest()
         {
             if (_loadCubeRequest != null)
             {
                 _loadCubeRequest.Cancelled = true;
+                _loadCubeRequest = null;
             }
+        }
 
-            if (_cubes != null)
+        private void RemoveChildModelObjects()
+        {
+            foreach (var cube in _cubes)
             {
-                foreach (var cube in _cubes)
-                {
-                    ReleaseCubeGameObject(cube);
-                }
-                _cubes.Clear();
+                ReleaseCubeGameObject(cube);
             }
+            _cubes.Clear();
+        }
 
+        private void RemoveChildDetectors()
+        {
             foreach (var detector in _childDetectors)
             {
                 detector.GetComponent<IsRendered>().DestroyChildren();
                 ReleaseDetectorCube(detector);
             }
             _childDetectors.Clear();
+        }
+
+
+        private void DestroyChildren()
+        {
+            CancelRequest();
+
+            RemoveChildModelObjects();
+
+            RemoveChildDetectors();
+
             _upgraded = false;
+            _upgrading = false;
+        }
+
+        private IEnumerator WaitForChildrenToLoad(IEnumerable<GameObject> newDetectors)
+        {
+
+            while (newDetectors.Any(cd =>
+            {
+                var cdAsIsRendered = cd.GetComponent<IsRendered>();
+                return cdAsIsRendered._loadCubeRequest != null;
+            }))
+            {
+                yield return null;
+                if (ShouldHideModel)
+                {
+                    _meshRenderer.enabled = true;
+                    DestroyChildren();
+                    break;
+                }
+            }
+        }
+
+        private IEnumerator DestroyChildrenAfterLoading(IEnumerable<GameObject> childDetectors)
+        {
+            yield return null;
+            RemoveChildDetectors();
+            _childDetectors.AddRange(childDetectors);
+            yield return StartCoroutine(WaitForChildrenToLoad(_childDetectors));
+            RemoveChildModelObjects();
+            _upgrading = false;
+            _upgraded = true;
+        }
+
+        public bool ShouldHideModel
+        {
+            get { return !GeometryUtility.TestPlanesAABB(_manager.CameraFrustrum, _render.bounds); }
         }
 
         private IEnumerator StopRenderCheck(Camera cameraToCheckAgainst)
         {
             while (true)
             {
-                if (!GeometryUtility.TestPlanesAABB(_manager.CameraFrustrum, _render.bounds))
+                if (ShouldHideModel)
                 {
                     _meshRenderer.enabled = true;
                     DestroyChildren();
@@ -148,17 +204,14 @@
                 }
                 if (Upgradable && ShouldUpgrade(cameraToCheckAgainst))
                 {
+                    _upgrading = true;
                     yield return
                         StartCoroutine(_manager.AddUpgradedDetectorCubes(_pyriteQuery, _x, _y, _z, _lod,
                             addedDetectors =>
                             {
-                                DestroyChildren();
-                                _childDetectors.AddRange(addedDetectors);
-                                _upgraded = true;
+                                StartCoroutine(DestroyChildrenAfterLoading(addedDetectors));
                             }));
-                }
-
-                if (Downgradable && !ShouldUpgrade(cameraToCheckAgainst))
+                }else if (Downgradable && !ShouldUpgrade(cameraToCheckAgainst))
                 {
                     DestroyChildren();
                     yield return StartCoroutine(RequestCubeLoad());
