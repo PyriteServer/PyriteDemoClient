8446017d4ed8fd0eeb00dc492aff3579d246463e
Stefan Gordon <stefango@microsoft.com>
2015-05-13 09:50:28 -0700

    Demo magic.

--- Assets/Cube Loader/src/InputManager.cs
+++ Assets/Cube Loader/src/InputManager.cs
@@ -21,46 +21,46 @@ public class InputManager : MonoBehaviour
     {
         _pivotCamera = GetComponentInChildren<Camera>();
         _targetPosition = transform;
-    }
-
-    void Update()
-    {        
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			
-			// Get movement of the finger since last frame
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-			
-			// Move object across XY plane
-			_targetPosition.Translate (
-				-touchDeltaPosition.x * TouchTranslationDeltaRate, 
-				-touchDeltaPosition.y * TouchTranslationDeltaRate, 
-				0);
-		}
-		else
-		{
-			_moveX = Input.GetAxis ("Horizontal") * Time.deltaTime * TranslationDeltaRate;
-			_moveY = Input.GetAxis ("Vertical") * Time.deltaTime * TranslationDeltaRate;
-			_moveZ = Input.GetAxis ("Forward") * Time.deltaTime * TranslationDeltaRate;
-
-			_yaw += Input.GetAxis ("HorizontalTurn") * Time.deltaTime * RotationDeltaRate;
-			_camPitch += Input.GetAxis ("VerticalTurn") * Time.deltaTime * RotationDeltaRate;
-
-			if (Input.GetButton ("XboxLB")) {
-				_moveY -= Time.deltaTime * TranslationDeltaRate;
-			}
-			if (Input.GetButton ("XboxRB")) {
-				_moveY += Time.deltaTime * TranslationDeltaRate;
-			}
-
-
-			_targetPosition.Translate (Vector3.up * _moveY, Space.World);
-			_targetPosition.Translate (Vector3.forward * _moveZ + Vector3.right * _moveX, Space.Self);  
-		}
-    }
+    }    
 
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, _targetPosition.position, 1f);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            _targetPosition.Translate(
+                -touchDeltaPosition.x * TouchTranslationDeltaRate,
+                -touchDeltaPosition.y * TouchTranslationDeltaRate,
+                0);
+        }
+        else
+        {
+            _moveX = Input.GetAxis("Horizontal") * Time.deltaTime * TranslationDeltaRate;
+            _moveY = Input.GetAxis("Vertical") * Time.deltaTime * TranslationDeltaRate;
+            _moveZ = Input.GetAxis("Forward") * Time.deltaTime * TranslationDeltaRate;
+
+            _yaw += Input.GetAxis("HorizontalTurn") * Time.deltaTime * RotationDeltaRate;
+            _camPitch += Input.GetAxis("VerticalTurn") * Time.deltaTime * RotationDeltaRate;
+
+            if (Input.GetButton("XboxLB"))
+            {
+                _moveY -= Time.deltaTime * TranslationDeltaRate;
+            }
+            if (Input.GetButton("XboxRB"))
+            {
+                _moveY += Time.deltaTime * TranslationDeltaRate;
+            }
+
+
+            _targetPosition.Translate(Vector3.up * _moveY, Space.World);
+            _targetPosition.Translate(Vector3.forward * _moveZ + Vector3.right * _moveX, Space.Self);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, _targetPosition.position, Time.time);
 
         _rigOrientation.eulerAngles = new Vector3(0, LimitAngles(_yaw), 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, _rigOrientation, Time.time);
--- Assets/Demo/InputManagerVcr.cs
+++ Assets/Demo/InputManagerVcr.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
+using System;
 
 public class InputManagerVcr : MonoBehaviour
 {
@@ -22,6 +23,8 @@ public class InputManagerVcr : MonoBehaviour
     public string VcrFilename;
     private bool useVCR;
     private InputVCR vcr;
+
+    private bool epicInitiated = false;
 
     void Awake()
     {
@@ -58,44 +61,56 @@ public class InputManagerVcr : MonoBehaviour
         _targetPosition = transform;        
     }    
 
-    void Update()
-    {        
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			
-			// Get movement of the finger since last frame
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-			
-			// Move object across XY plane
-			_targetPosition.Translate (
-				-touchDeltaPosition.x * TouchTranslationDeltaRate, 
-				-touchDeltaPosition.y * TouchTranslationDeltaRate, 
-				0);
-		}
-		else
-		{
-			_moveX = vcr.GetAxis ("Horizontal") * Time.deltaTime * TranslationDeltaRate;
-			_moveY = vcr.GetAxis ("Vertical") * Time.deltaTime * TranslationDeltaRate;
-			_moveZ = vcr.GetAxis ("Forward") * Time.deltaTime * TranslationDeltaRate;
-
-			_yaw += vcr.GetAxis ("HorizontalTurn") * Time.deltaTime * RotationDeltaRate;
-			_camPitch += vcr.GetAxis ("VerticalTurn") * Time.deltaTime * RotationDeltaRate;
-
-			if (vcr.GetButton ("XboxLB")) {
-				_moveY -= Time.deltaTime * TranslationDeltaRate;
-			}
-			if (vcr.GetButton ("XboxRB")) {
-				_moveY += Time.deltaTime * TranslationDeltaRate;
-			}
-
-
-			_targetPosition.Translate (Vector3.up * _moveY, Space.World);
-			_targetPosition.Translate (Vector3.forward * _moveZ + Vector3.right * _moveX, Space.Self);  
-		}
-    }
-
     void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, _targetPosition.position, 1f);
+        if (!epicInitiated && Input.GetButton("Fire1") || Input.GetButton("XboxA"))
+        {
+            epicInitiated = true;
+            EpicFail();
+        }
+
+        if (!epicInitiated && Input.GetButton("Fire2") || Input.GetButton("XboxB"))
+        {
+            epicInitiated = false;
+            EpicUnFail();            
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            _targetPosition.Translate(
+                -touchDeltaPosition.x * TouchTranslationDeltaRate,
+                -touchDeltaPosition.y * TouchTranslationDeltaRate,
+                0);
+        }
+        else
+        {
+            _moveX = vcr.GetAxis("Horizontal") * Time.deltaTime * TranslationDeltaRate;
+            _moveY = vcr.GetAxis("Vertical") * Time.deltaTime * TranslationDeltaRate;
+            _moveZ = vcr.GetAxis("Forward") * Time.deltaTime * TranslationDeltaRate;
+
+            _yaw += vcr.GetAxis("HorizontalTurn") * Time.deltaTime * RotationDeltaRate;
+            _camPitch += vcr.GetAxis("VerticalTurn") * Time.deltaTime * RotationDeltaRate;
+
+            if (vcr.GetButton("XboxLB"))
+            {
+                _moveY -= Time.deltaTime * TranslationDeltaRate;
+            }
+            if (vcr.GetButton("XboxRB"))
+            {
+                _moveY += Time.deltaTime * TranslationDeltaRate;
+            }
+
+
+            _targetPosition.Translate(Vector3.up * _moveY, Space.World);
+            _targetPosition.Translate(Vector3.forward * _moveZ + Vector3.right * _moveX, Space.Self);
+        }
+
+        transform.position = Vector3.Lerp(transform.position, _targetPosition.position, Time.time);
 
         _rigOrientation.eulerAngles = new Vector3(0, LimitAngles(_yaw), 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, _rigOrientation, Time.time);
@@ -105,6 +120,30 @@ public class InputManagerVcr : MonoBehaviour
         var planePoint = transform.position;
         planePoint.y = 0;
         Debug.DrawLine(transform.position, planePoint, Color.green, 0f, true);
+    }
+
+    private void EpicFail()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("MeshCube");
+        foreach (var go in objects)
+        {
+            var collider = go.AddComponent<BoxCollider>();
+            var rigid = go.AddComponent<Rigidbody>();
+            if (rigid != null)
+                rigid.mass = 1000;
+        }
+    }
+
+    private void EpicUnFail()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("MeshCube");
+        foreach (var go in objects)
+        {
+            Destroy(go.GetComponent<BoxCollider>());
+            Destroy(go.GetComponent<Rigidbody>());
+            go.transform.position = Vector3.zero;
+            go.transform.rotation = Quaternion.identity;
+        }
     }
 
     private static float LimitAngles(float angle)
