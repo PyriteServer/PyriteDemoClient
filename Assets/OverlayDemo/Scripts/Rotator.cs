cfc0ef9664edce9d622fa5a9470dbeedd853a95c
Ritchie Lozada <rlozada@microsoft.com>
2015-06-03 22:19:26 -0700

    Selector Prefab and Overlay Set (2 Building Selectors and Flight Path)

---
+++ Assets/OverlayDemo/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float TurnRate = 90f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * TurnRate);
+    }
+}
