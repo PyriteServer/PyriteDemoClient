67e66392b5eb5a227923b742f27dc187a260b6e9
Ritchie Lozada <rlozada@microsoft.com>
2015-06-04 13:00:17 -0700

    Demo Heli Model

---
+++ Assets/OverlayDemo/Scripts/Rotator2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator2 : MonoBehaviour
+{
+    public float TurnRate = 90f;
+    public Vector3 Rotation;
+
+    void Update()
+    {
+        transform.Rotate(Rotation * Time.deltaTime * TurnRate);
+    }
+}
