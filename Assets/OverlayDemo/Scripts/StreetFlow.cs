e1c93aced913c744cbfa3733c411475eb53159f1
Ritchie Lozada <rlozada@microsoft.com>
2015-06-05 16:19:29 -0700

    Support for texture offset flow on street route indicator

---
+++ Assets/OverlayDemo/Scripts/StreetFlow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreetFlow : MonoBehaviour
+{
+    public float scrollSpeed = 1f;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+    }
+}
