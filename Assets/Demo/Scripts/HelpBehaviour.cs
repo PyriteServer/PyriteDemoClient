3c13f6bbd36acd85665c692e16af9ef76211e704
Andre Price <obsoleted@users.noreply.github.com>
2015-09-08 10:09:02 -0700

    Add simple help dialog
    - Displays for 5 seconds initially
    - Toggled with help button

---
+++ Assets/Demo/Scripts/HelpBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpBehaviour : MonoBehaviour {
+
+    public GameObject HelpPanel;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    void OnEnable()
+    {
+        StartCoroutine(DisableAfterTime(5, HelpPanel));
+    }
+
+    IEnumerator DisableAfterTime(int secondsToDisableAfter, GameObject objectToDisable)
+    {
+        yield return new WaitForSeconds(secondsToDisableAfter);
+        objectToDisable.SetActive(false);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void Toggle()
+    {
+        HelpPanel.SetActive(!HelpPanel.activeInHierarchy);
+    }
+}
